552cfa4409c105203a5ca9ba1edf74397b4890db
Agustin Remesal <60765936+agustinremesal1990@users.noreply.github.com>
2020-03-08 15:09:13 -0300

    Add files via upload

---
+++ React-Native-App/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import AppNavigator from "./navigator/AppNavigator";
+// import ApolloClient from "apollo-boost";
+// import { ApolloProvider } from "react-apollo";
+
+// const client = new ApolloClient({
+//   uri: "https://graphql.contentful.com/content/v1/spaces/ldcl3ayg0mhx",
+//   credentials: "same-origin",
+//   headers: {
+//     Authorization: `Bearer 93f3808c25c1f5bdb95476ca8576c6eaa12b5587efb956efb242ceead7cb3840`
+//   }
+// });
+
+const initialState = {
+  action: "",
+  name: ""
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "OPEN_MENU":
+      return { action: "openMenu" };
+    case "CLOSE_MENU":
+      return { action: "closeMenu" };
+    case "UPDATE_NAME":
+      return { name: action.name };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+const App = () => (
+  <Provider store={store}>
+    <AppNavigator />
+  </Provider>
+);
+
+export default App;
